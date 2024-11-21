71cdf0c3dfef2f2b6dabd2e4216d70550a99b140
JennaSigman <123jello@gmail.com>
2024-11-21 17:23:43 -0500

    first commit

---
+++ Kanbas/Enrollments/dao.js
@@ -0,0 +1,20 @@
+import Database from "../Database/index.js";
+
+export function enrollUserInCourse(userId, courseId) {
+  const { enrollments } = Database;
+  const newEnrollment = { _id: Date.now().toString(), user: userId, course: courseId };
+  Database.enrollments = [...enrollments, newEnrollment];
+  return newEnrollment;
+}
+
+export function unenrollUserFromCourse(userId, courseId) {
+  const { enrollments } = Database;
+  Database.enrollments = enrollments.filter(
+    (enrollment) => !(enrollment.user === userId && enrollment.course === courseId)
+  );
+}
+
+export function findEnrollmentsForUser(userId) {
+  const { enrollments } = Database;
+  return enrollments.filter((enrollment) => enrollment.user === userId);
+}
